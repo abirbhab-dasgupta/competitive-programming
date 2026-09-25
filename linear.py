2f6c4cf0c878b6ac26e7b4b3f8e184b934553f3d
Abirbhab Dasgupta <codewithad24@gmail.com>
2026-09-25 10:14:42 +0530

    Few changes with case sensitive code part in student.py

--- linear.py
+++ linear.py
@@ -1,5 +1,3 @@
-# Linear Search
-# Searches for an element one by one
 arr = [10, 20, 30, 40, 50]
 key = int(input("Enter element to search: "))
 for i in range(len(arr)):
--- student.py
+++ student.py
@@ -5,23 +5,11 @@ for i in range(n):
     students.append(name)
 search_name = input("Enter student name to search: ")
 found = False
-for i in range(n):
-    if students[i] == search_name:
-        print("\nCase-sensitive search:")
-        print("Student found at position:",i+1)
-        found = True
-        break
-if not found:
-    print("\nCase-sensitive search:")
-    print("Student not found at position:",n+1)
-found = False
 
 for i in range(n):
     if students[i].lower() == search_name.lower():
-        print("\nCase-sensitive search:")
         print("Student found at position:",i+1)
         found = True
         break
 if not found:
-    print("\nCase-sensitive search:")
     print("Student not found at position:",n+1)
